9c194e99547b52edd3f03152abd4cda1022eafc1
soumyajyoti mohanta <soumyajyotimohanta@gmail.com>
2025-04-17 18:07:08 +0530

    try again

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import AppExpress from "@itznotabug/appexpress";
 const app = new AppExpress();
 
-app.views('views');
-// make sure to add correct content-types.
-app.get('/', (request, response) => response.render('index', { title: 'Home' }));
+// Remove the views setup as it's not supported
+// app.views('views');
 
+// Use send() instead of render()
+app.get('/', (request, response) => {
+    response.send('<html><head><title>Home</title></head><body><h1>Welcome Home</h1></body></html>');
+});
+
+// If you need to serve JSON:
+app.get('/api', (request, response) => {
+    response.json({ message: 'Hello from API', status: 'success' });
+});
 
 export default async (context) => await app.attach(context);
