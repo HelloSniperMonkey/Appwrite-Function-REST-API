0dce026fe74067bb88a61ca06bb7954e35114c8d
soumyajyoti mohanta <soumyajyotimohanta@gmail.com>
2025-04-17 17:55:52 +0530

    lets see

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import AppExpress from "@itznotabug/appexpress";
 const app = new AppExpress();
 
-app.use(AppExpress.static('public'));
-
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+app.use(app.static('public'));
+app.views('views');
+// make sure to add correct content-types.
+app.get('/', (request, response) => response.binary('index.html'));
+// app.get("/", (req, res) => {
+//     res.send("Hello World!");
+// });
 
 export default async (context) => await app.attach(context);
