41a3bd103bbef28f9536ff9ea9b4222f6d2de030
soumyajyoti mohanta <soumyajyotimohanta@gmail.com>
2025-04-17 17:00:09 +0530

    see

--- src/main.js
+++ src/main.js
@@ -1,15 +1,8 @@
 import AppExpress from "@itznotabug/appexpress";
 const app = new AppExpress();
-import userRoutes from "./routes/user.js";
-import authMiddleware from "./middleware/auth.js";
 
-app.middleware(authMiddleware);
-app.use("/user", userRoutes);
-
-const getRoutes = (request, response) => {
-    response.json({ routes: ["/", "/user/:username", "/user"] });
-};
-
-app.get("/", getRoutes);
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
 export default async (context) => await app.attach(context);
