f7ab715afe2fc502e6057dd40c3fcb88eee3b788
soumyajyoti mohanta <soumyajyotimohanta@gmail.com>
2025-04-17 18:12:07 +0530

    html ?

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import AppExpress from "@itznotabug/appexpress";
 const app = new AppExpress();
 
-// Remove the views setup as it's not supported
-// app.views('views');
-
-// Use send() instead of render()
+// Use send() with appropriate content type header
 app.get('/', (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
     response.send('<html><head><title>Home</title></head><body><h1>Welcome Home</h1></body></html>');
 });
 
